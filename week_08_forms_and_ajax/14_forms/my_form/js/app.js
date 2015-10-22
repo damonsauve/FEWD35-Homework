e7c2bd1de0d09bfa73f47e70114452342ff0639f
Damon Sauve <damonsauve@gmail.com>
2015-10-22 12:33:17 -0700

    added error messaging for validation.

--- week_08_forms_and_ajax/14_forms/my_form/js/app.js
+++ week_08_forms_and_ajax/14_forms/my_form/js/app.js
@@ -5,27 +5,47 @@ $(document).ready(function() {
         event.preventDefault();
 
         var formInfo = {
+
+            // Using IDS
+            //
             first_name: $("#first-name").val(),
             last_name:  $("#last-name").val(),
             email:      $("#email-address").val(),
             address:    $("#address").val(),
             address_2:  $("#address-2").val(),
             city:       $("#city").val(),
-            state:      $("#state").val()
+            state:      $("#state").val(),
+            country:    $("#country").val()
+
+            // // using name attribute.
+            // //
+            // first_name: $("input[name='first-name']").val(),
+            // last_name:  $("input[name='last-name']").val(),
+            // email:      $("input[name='email-address']").val(),
+            // address:    $("input[name='address']").val(),
+            // address_2:  $("input[name='address-2']").val(),
+            // city:       $("input[name='city']").val(),
+            // state:      $("select[name='state']").val(),
+            // country:    $("select[name='country']").val()
         };
 
-        console.log('name: ' + formInfo.name);
+        console.log(formInfo.first_name);
         console.log(formInfo);
 
         for (var key in formInfo) {
 
             if (formInfo[key] === '') {
-                alert('Please fill in all fields!');
+                // alert('Please fill in all fields!');
+
+                $( "#messages" ).text('ERROR: ' + key + ' field is empty');
+
                 return false;
             }
         }
 
-        alert('Form submitted!');
+
+        // document.forms["my-form"].submit();
+        $("#my-form")[0].submit();
 
     });
 
